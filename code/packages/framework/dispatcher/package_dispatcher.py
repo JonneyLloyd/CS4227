73b332e854906f95147a7d952a224518dbccc3fc
JonneyLloyd <jonneylloyd@yahoo.co.uk>
2017-11-04 18:40:25 +0000

    Fixed dispatcher issues

--- code/packages/framework/dispatcher/package_dispatcher.py
+++ code/packages/framework/dispatcher/package_dispatcher.py
@@ -4,14 +4,14 @@ from ..context import PackageContext
 from ..interceptor import PackageInterceptor
 
 
-class SourceDispatcher():
+class PackageDispatcher():
 
     def __init__(self) -> None:
         self._interceptors: List[PackageInterceptor] = []
 
     def dispatch(self, context: PackageContext) -> None:
         for interceptor in self._interceptors:
-            interceptor.on_source(context)
+            interceptor.on_package(context)
 
     def register(self, interceptor: PackageInterceptor) -> None:
         self._interceptors.append(interceptor)
--- code/packages/framework/dispatcher/storage_dispatcher.py
+++ code/packages/framework/dispatcher/storage_dispatcher.py
@@ -4,14 +4,14 @@ from ..context import StorageContext
 from ..interceptor import StorageInterceptor
 
 
-class SourceDispatcher():
+class StorageDispatcher():
 
     def __init__(self) -> None:
         self._interceptors: List[StorageInterceptor] = []
 
     def dispatch(self, context: StorageContext) -> None:
         for interceptor in self._interceptors:
-            interceptor.on_source(context)
+            interceptor.on_storage(context)
 
     def register(self, interceptor: StorageInterceptor) -> None:
         self._interceptors.append(interceptor)
