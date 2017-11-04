1f7dd7f740cfa16a0eac2cb139180e0d762618e1
JonneyLloyd <jonneylloyd@yahoo.co.uk>
2017-11-04 18:40:25 +0000

    Fixed dispatcher issues

--- code/packages/framework/dispatcher/build_dispatcher.py
+++ code/packages/framework/dispatcher/build_dispatcher.py
@@ -4,14 +4,14 @@ from ..context import BuildContext
 from ..interceptor import BuildInterceptor
 
 
-class SourceDispatcher():
+class BuildDispatcher():
 
     def __init__(self) -> None:
         self._interceptors: List[BuildInterceptor] = []
 
     def dispatch(self, context: BuildContext) -> None:
         for interceptor in self._interceptors:
-            interceptor.on_source(context)
+            interceptor.on_build(context)
 
     def register(self, interceptor: BuildInterceptor) -> None:
         self._interceptors.append(interceptor)
--- code/packages/framework/dispatcher/deploy_dispatcher.py
+++ code/packages/framework/dispatcher/deploy_dispatcher.py
@@ -4,14 +4,14 @@ from ..context import DeployContext
 from ..interceptor import DeployInterceptor
 
 
-class SourceDispatcher():
+class DeployDispatcher():
 
     def __init__(self) -> None:
         self._interceptors: List[DeployInterceptor] = []
 
     def dispatch(self, context: DeployContext) -> None:
         for interceptor in self._interceptors:
-            interceptor.on_source(context)
+            interceptor.on_deploy(context)
 
     def register(self, interceptor: DeployInterceptor) -> None:
         self._interceptors.append(interceptor)
