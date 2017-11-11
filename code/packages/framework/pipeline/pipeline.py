b65910a5d9f347fbae545fe8afb149ad6e626808
Russell Hickey <RHickeyJnr@gmail.com>
2017-11-11 20:09:57 +0000

    Rebase

--- code/packages/framework/pipeline/pipeline.py
+++ code/packages/framework/pipeline/pipeline.py
@@ -1,10 +1,10 @@
 from typing import List
 
-from framework.context import SourceContext
-from framework.dispatcher import SourceDispatcher
 from framework.pipeline import PipelineBase
 from framework.pipeline.pipeline_memento import PipelineMemento
 from framework.config import ConfigMemento, ConfigModel
+from framework.context import SourceContext, BuildContext, PackageContext, DeployContext
+from framework.dispatcher import SourceDispatcher, BuildDispatcher, PackageDispatcher, DeployDispatcher
 from framework.control import ModuleRegistry
 
 # Define our own type annotations
@@ -13,6 +13,9 @@ ConfigMementoList = List[ConfigMemento]
 class Pipeline(PipelineBase):
     def __init__(self) -> None:
         self._source_dispatcher: SourceDispatcher = SourceDispatcher()
+        self._build_dispatcher: BuildDispatcher = BuildDispatcher()
+        self._package_dispatcher: PackageDispatcher = PackageDispatcher()
+        self._deploy_dispatcher: DeployDispatcher = DeployDispatcher()
 
     def _create_config_and_interceptor(self, memento: ConfigMemento):
         con_int_tup = ModuleRegistry.get_module(memento.config['concrete_key'])
@@ -27,8 +30,23 @@ class Pipeline(PipelineBase):
     def source_dispatcher(self) -> SourceDispatcher:
         return self._source_dispatcher
 
+    @property
+    def build_dispatcher(self) -> BuildDispatcher:
+        return self._build_dispatcher
+
+    @property
+    def package_dispatcher(self) -> PackageDispatcher:
+        return self._package_dispatcher
+
+    @property
+    def deploy_dispatcher(self) -> DeployDispatcher:
+        return self._deploy_dispatcher
+
     def execute(self) -> None:
         self._source_dispatcher.dispatch(SourceContext(self))
+        self._build_dispatcher.dispatch(BuildContext(self))
+        self._package_dispatcher.dispatch(PackageContext(self))
+        self._deploy_dispatcher.dispatch(DeployContext(self))
 
     def set_memento(self, memento: PipelineMemento) -> None:
         self.config = [self._create_config_and_interceptor(config_memento)
