241c07612a825459147921112e1dde00f28cd3fe
Jay Conroy <jayryanconroy@gmail.com>
2017-11-11 14:04:04 +0000

    Add set_state for context objects

--- code/client/src_japp/run.py
+++ code/client/src_japp/run.py
@@ -1,5 +1,6 @@
 import spur
 
 shell = spur.LocalShell()
+""" Replace 'jay' below with your own username"""
 shell.run(['sh', '-c', 'cd /home/jay/fw_test; touch TEST_DEPLOY_FILE'])
 print('hello world')
